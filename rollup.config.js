36c192fad411cb7a8d9ca7ff7fcc2bc58a86f3ee
nateshmbhat <nateshmbhat1@gmail.com>
2021-04-01 20:09:08 +0530

    upgraded and fixes issues

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,20 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import svelte from 'rollup-plugin-svelte'
+import autoPreprocess from 'svelte-preprocess'
+import { terser } from 'rollup-plugin-terser'
 
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import svelte from 'rollup-plugin-svelte';
-import autoPreprocess from 'svelte-preprocess';
-import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json'
 
-import pkg from './package.json';
-
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH
 const name = pkg.name
   .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-  .replace(/^\w/, m => m.toUpperCase())
-  .replace(/-\w/g, m => m[1].toUpperCase());
+  .replace(/^\w/, (m) => m.toUpperCase())
+  .replace(/-\w/g, (m) => m[1].toUpperCase())
 
 export default {
-  input: 'src/index.js',
+  input: 'src/main.ts',
   output: [
     {
       file: pkg.module,
@@ -33,20 +32,21 @@ export default {
     commonjs(),
     typescript(),
     svelte({
-      dev: !production,
-      css: css => {
-        css.write('index.css', production);
+      compilerOptions: {
+        dev: !production,
+        css: (css) => {
+          css.write('index.css', production)
+        },
       },
+      emitCss: false,
       preprocess: autoPreprocess(),
     }),
     resolve({
-      dedupe: [
-        'svelte',
-      ],
+      dedupe: ['svelte'],
     }),
     production && terser(),
   ],
   watch: {
     clearScreen: false,
   },
-};
+}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+export { default as AceEditor } from './AceEditor.svelte';
+import { Editor, Anchor, Annotation, Document } from "brace";
+export { Editor, Anchor, Document }
+export type { Annotation }
