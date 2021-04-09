b3941786e07d4c1c03157002873329531dc64e93
nateshmbhat <nateshmbhat1@gmail.com>
2021-04-09 05:39:25 +0530

    Added readonly option added copy-paste and cut-paste logic

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -4,24 +4,35 @@
   import "brace/theme/chrome";
 
   let text = "";
+  let readonly: boolean = false;
+  let visible: boolean = true;
 </script>
 
 <AceEditor
   on:selectionChange={(obj) => console.log(obj.detail)}
   on:paste={(obj) => console.log(obj.detail)}
-  on:input={(obj) => console.log(obj.detail)}
-  on:focus={() => console.log('focus')}
+  on:input={(obj) => {
+    text = obj.detail;
+    console.log(obj.detail);
+  }}
+  on:focus={() => console.log("focus")}
   on:documentChange={(obj) => console.log(`document change : ${obj.detail}`)}
-  on:cut={() => console.log('cut')}
-  on:cursorChange={() => console.log('cursor change')}
-  on:copy={() => console.log('copy')}
+  on:cut={() => console.log("cut")}
+  on:cursorChange={() => console.log("cursor change")}
+  on:copy={() => console.log("copy")}
   on:init={(editor) => console.log(editor.detail)}
   on:commandKey={(obj) => console.log(obj.detail)}
   on:changeMode={(obj) => console.log(`change mode : ${obj.detail}`)}
-  on:blur={() => console.log('blur')}
+  on:blur={() => console.log("blur")}
   width="100%"
   height="500px"
   lang="json"
   theme="chrome"
   value={text}
+  {readonly}
 />
+
+<button
+  style="opacity:{readonly ? 1 : 0.5}"
+  on:click={(e) => (readonly = !readonly)}>Toggle Readonly</button
+>
